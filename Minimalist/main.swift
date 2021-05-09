3c322dbfb6f79600e1b901a2a3d4df856d3d758a
Weera Wu <ing@oozou.com>
2021-05-09 15:00:00 +0700

    Initial commit

---
+++ Minimalist/main.swift
@@ -0,0 +1,90 @@
+//
+//  main.swift
+//  Minimalist
+//
+//  Created by Weera Wu on 9/5/2564 BE.
+//
+
+import Cocoa
+
+
+/// Case 1. Status item-based application
+let app = NSApplication.shared
+
+let statusBar = NSStatusBar.system
+let statusItem = statusBar.statusItem(withLength: NSStatusItem.variableLength)
+statusItem.button?.title = "😊"
+statusItem.button?.action = #selector(NSApplication.terminate)
+
+app.run()
+
+
+///// Case 2. Minimal windowed application
+//let app = NSApplication.shared
+//
+//let window = NSWindow.init(contentRect: NSMakeRect(0, 0, 320, 200), styleMask: .titled, backing: .buffered, defer: false)
+//window.orderFrontRegardless()
+//
+//app.run()
+
+
+///// Case 3: Fully activated Cocoa windowed app
+//let app = NSApplication.shared
+//app.setActivationPolicy(.regular)
+//
+//let window = NSWindow.init(contentRect: NSMakeRect(0, 0, 320, 200), styleMask: .titled, backing: .buffered, defer: false)
+//window.orderFrontRegardless()
+//
+//app.activate(ignoringOtherApps: true)
+//app.run()
+
+
+///// Case 4. Closing the app and cleanup
+//class AppDelegate: NSObject, NSApplicationDelegate {
+//    var mainWindow: NSWindow?
+//
+//    func applicationDidFinishLaunching(_ notification: Notification) {
+//        let window = NSWindow.init(contentRect: NSMakeRect(0, 0, 320, 200), styleMask: [.titled, .closable], backing: .buffered, defer: true)
+//        window.orderFrontRegardless()
+//        mainWindow = window
+//
+//        let app = NSApplication.shared
+//        app.activate(ignoringOtherApps: true)
+//    }
+//
+//    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+//        return true
+//    }
+//}
+//
+//let app = NSApplication.shared
+//app.setActivationPolicy(.regular)
+//
+//let delegate = AppDelegate()
+//app.delegate = delegate
+//app.run()
+
+
+///// Case 5. Menu item-based application
+//let app = NSApplication.shared
+//app.setActivationPolicy(.regular)
+//
+//let menubar = NSMenu()
+//let appMenuItem = NSMenuItem()
+//menubar.addItem(appMenuItem)
+//app.mainMenu = menubar
+//
+//let appMenu = NSMenu()
+//let appName = ProcessInfo.processInfo.processName
+//let quitTitle = "Quit \(appName)"
+//let quitMenuItem = NSMenuItem.init(title: quitTitle, action: #selector(NSApplication.terminate), keyEquivalent: "q")
+//appMenu.addItem(quitMenuItem)
+//appMenuItem.submenu = appMenu
+//
+//let window = NSWindow.init(contentRect: NSMakeRect(0, 0, 320, 200), styleMask: .titled, backing: .buffered, defer: false)
+//window.cascadeTopLeft(from: NSMakePoint(20, 20))
+//window.title = appName
+//window.makeKeyAndOrderFront(nil)
+//
+//app.activate(ignoringOtherApps: true)
+//app.run()
---
+++ README.md
@@ -0,0 +1,19 @@
+# Minimalist
+
+A collection of minimal Cocoa applications written in Swift.
+
+## Usage
+
+Uncomment a code block in the [main.swift](Minimalist/main.swift) file.
+
+### Xcode
+
+Click the Run button to build and run the app.
+
+### Command Line
+
+    $ cd Minimalist
+    $ swift main.swift
+
+    $ swiftc main.swift -o Minimalist
+    $ ./Minimalist
